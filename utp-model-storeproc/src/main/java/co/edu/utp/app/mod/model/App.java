bec312c6ae596f8e8885369f61cb01c2387ddb2e
Wilman Tello <wilmandx@gmail.com>
2012-10-18 17:48:18 -0500

    Prueba de commit
    ok

---
+++ utp-model-storeproc/src/main/java/co/edu/utp/app/mod/model/App.java
@@ -0,0 +1,17 @@
+package co.edu.utp.app.mod.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World WIl!" );
+    }
+    
+    public int sumar(int x,int y){
+    	return x+y;
+    }
+}
